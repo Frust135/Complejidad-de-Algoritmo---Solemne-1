54cf32c36d9df2942b49df2b5095b8e2f9e861dd
Santiago <santiagosaav.99@gmail.com>
2020-09-23 13:56:20 -0300

    Cambios pequeños en isOrden. Se ha definido la función sortSelección
    Se han realizado pequeños cambios en la función isOrden, y por otro lado se ha inicializado lo que corresponde a la función de ordenamiento de tipo Selección.

--- libreriaOrdenamientos.py
+++ libreriaOrdenamientos.py
@@ -1,9 +1,11 @@
-def ordenado (arreglo):
+#__Algoritmo que retorna si un arreglo se encuentra ordenado o no__
+def isOrdenado (arreglo):
     longitud_arreglo=len(arreglo)
     for x in range(0, longitud_arreglo):
         for y in range(x+1, longitud_arreglo):
             if arreglo[x] > arreglo[y]: return False
     return True
-
-arreglo=[3,1,1,1]
-print(ordenado(arreglo))
+#__Algoritmo de Selección__
+def sortSeleccion(arreglo):
+    if isOrdenado(arreglo): return arreglo
+    
